497498cacaba02c4810f8af8d9e3ca3fb20167ce
Alejandro Marin Pons <anafralex@hotmail.com>
2016-02-23 14:04:25 +0100

    Pelota que rebota MAL

---
+++ Pang/Assets/Scripts/ParabolaBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ParabolaBehaviour : MonoBehaviour
+{
+    private float xSpeed = 2f;
+    private float ySpeed = 1f;
+    private float acceleration = -0.5f;
+
+    void Update()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(xSpeed, ySpeed);
+        ySpeed += acceleration;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Suelo")
+        {
+            ySpeed *= -1;
+        }
+        if (col.gameObject.tag == "Pared")
+        {
+            xSpeed *= -1;
+        }
+        if (col.gameObject.tag == "Plataforma")
+        {
+
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "suelo")
+        {
+            ySpeed = 1f;
+        }
+    }
+}
