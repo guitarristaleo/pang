49c9e67a139fb2beebd4ad4c67736c98b39e328d
guitarristaleo <guitarristaleo@hotmail.com>
2016-02-23 11:36:37 +0100

    asdf
    asdf

---
+++ Pang/Assets/Scripts/balaPang.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class balaPang : MonoBehaviour {
+
+    public bool disparado = false;
+    public float velocidad = 5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(disparado)
+        {
+            //GetComponent<Rigidbody2D>().velocity = new Vector2(0, velocidad);
+        }
+	}
+}
---
+++ Pang/Assets/Scripts/playerPang.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerPang : MonoBehaviour
+{
+
+    public float fuerzaSalto = 5;
+    public float fuerzaLateral = 5;
+    public float velocidadMaxima = 5;
+    public int puntos = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, fuerzaSalto);
+            //GetComponent<Rigidbody2D>().AddForce(Vector2.up * fuerzaSalto);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(fuerzaLateral, GetComponent<Rigidbody2D>().velocity.y);
+            //GetComponent<Rigidbody2D>().AddForce(Vector2.right * fuerzaLateral);
+            //if(GetComponent<Rigidbody2D>().velocity.x > velocidadMaxima)
+            //{
+            //    GetComponent<Rigidbody2D>().velocity = new Vector2(velocidadMaxima, GetComponent<Rigidbody2D>().velocity.y);
+            //}
+        }
+        else if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-1 * fuerzaLateral, GetComponent<Rigidbody2D>().velocity.y);
+            //GetComponent<Rigidbody2D>().AddForce(Vector2.left * fuerzaLateral);
+            //if (GetComponent<Rigidbody2D>().velocity.x < -velocidadMaxima)
+            //{
+            //    GetComponent<Rigidbody2D>().velocity = new Vector2(-velocidadMaxima, GetComponent<Rigidbody2D>().velocity.y);
+            //}
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
+        
+    }
+}
